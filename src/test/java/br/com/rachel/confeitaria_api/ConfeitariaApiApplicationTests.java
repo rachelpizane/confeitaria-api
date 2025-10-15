df49913e10974ba56f136740c586979771ea43be
Rachel Pizane <rachel.pzm@gmail.com>
2025-10-15 20:46:37 -0300

    first commit

---
+++ src/test/java/br/com/rachel/confeitaria_api/ConfeitariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rachel.confeitaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
